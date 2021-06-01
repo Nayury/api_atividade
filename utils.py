fc44ea649b532161d751a4f4bfd3b53b4eb68133
Nayury <nayury_10@hotmail.com>
2021-05-31 23:51:10 -0300

    Alterado a REST API para realizar autenticação no banco de dados. Em models foi criado tabela de usuarios com login e senha.

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 def insere_pessoas():
     pessoa = Pessoas(nome='Dark', idade=23)
@@ -20,10 +20,19 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Dark').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
 
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
 
 if __name__ == '__main__':
+    insere_usuario('oi', '654')
+    insere_usuario('io', '456')
+    consulta_todos_usuarios()
     # insere_pessoas()
     # altera_pessoa()
-    exclui_pessoa()
-    consulta_pessoas()
+    # exclui_pessoa()
+    # consulta_pessoas()
